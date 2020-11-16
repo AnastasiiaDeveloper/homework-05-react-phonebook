4f8f75991a8dfb49a35b32daafe73b089d50830c
Anastasiia Sereda <nastiasereda.1994@gmail.com>
2020-11-16 20:46:50 +0200

    Updates

---
+++ precache-manifest.d77f237d5d4217edea2eb0a7eb008c1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eae1f2a5729fd2b0f6a6357a908fbd83",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "824febde0c395855da30",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.e0b33c20.chunk.css"
+  },
+  {
+    "revision": "ccf6bdb673210eca991e",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.27bf0a75.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.27bf0a75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "824febde0c395855da30",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.2e80d141.chunk.js"
+  },
+  {
+    "revision": "4677d0c8f4ea61260160",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime-main.d07c494f.js"
+  }
+]);
